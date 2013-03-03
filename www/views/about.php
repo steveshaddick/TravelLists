f3468da292d180b3f333aada635ce147b6dcd34e
Nathan <nathang87@gmail.com>
2013-03-03 15:08:26 -0500

    Revert "About page updated"
    This reverts commit 7a79965e81924472fdfdd0176bde923cfd42df4f.

--- www/views/about.php
+++ www/views/about.php
@@ -1,6 +1,6 @@
 <?php include(BASE_PATH . 'views/parts/header.php'); ?>
 
-
+<<<<<<< HEAD
 	<div id="body">
 		<div class="contentWrapper">
 		
@@ -12,7 +12,89 @@
 			
 			<section class="page-content about-page">
 
-				<p>Tripnotes dsfdsfds
+				<p>Tripnotes is the place to capture and organize travel ideas by yourself and with others.  Planning a trip can be overwhelming, so we built Trip Notes to help people get and organize suggestions.
+=======
+<style type="text/css">
+
+body{
+color: #151633;
+font-family: "chaparral-pro", georgia, serif;
+font-size: 24px;
+font-weight: 600;
+}
+
+.about-header{
+	width: 100%;
+	max-width: 1250px;
+	margin: 0 auto;
+	text-align: center;
+	margin-top: 50px;
+	margin-bottom: 50px;
+	}
+	
+.about-header .site-title {
+	  font-family: "pragmatica-web", helvetica, arial, sans-serif;
+	  font-weight: 600;
+	  font-size: 76px;
+	  line-height: 76px;
+	  color: #5f5c66;
+	}
+
+.about-header .site-subtitle {
+	  font-family: "chaparral-pro", georgia, serif;
+	  font-size: 23px;
+	  line-height: 23px;
+	  color: #b8b8b8;
+	}
+	
+#landingPage{
+	width: 72%;
+	margin: 0px auto;
+	padding-bottom: 50px;;
+}
+
+.about-header a:link{
+	color: #5f5c66;
+}
+
+.about-header a:hover{
+	text-decoration: none;
+}
+
+.about-header a:visited{
+	color: #5f5c66;
+}
+
+
+a:link{
+	color: #799b28;
+}
+
+a:visited{
+	color: #799b28;
+}
+	
+a:hover{
+	
+color: #a5bb77;
+text-decoration: underline;
+}
+
+</style>
+
+	<div id="body">
+		<div class="contentWrapper">
+		
+		<header class="about-header">
+			<h1 class="site-title"><a href="http://dev.maketripnotes.com">Tripnotes</a></h1>
+			<span class="site-subtitle">The easiest way to get &amp; organize trip suggestions.</span>
+		</header>		
+		
+		
+			<div id="landingPage" class="pageContent">
+
+				<p>Trip Notes is the place to capture and organize travel ideas by yourself and with others.  Planning a trip can be overwhelming, so we built Trip Notes to help people get and organize suggestions.
+>>>>>>> Updated About
 				</p>
 				
 				<p>Trip Notes is new, and we'd love your feedback on how to make it better.
@@ -28,8 +110,11 @@
 				
 
 
+<<<<<<< HEAD
 			</section>
-
+=======
+			</div>
+>>>>>>> Updated About
 		</div>
 	</div>
 
