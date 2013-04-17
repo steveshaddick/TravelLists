c81be4ba3b1b6174a8987e53a7ca07b1a39dc335
Nathan <nathang87@gmail.com>
2013-04-16 22:39:24 -0400

    Fixed footer link spacing

--- www/views/parts/footer.php
+++ www/views/parts/footer.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-	<a href="/about" >About</a><span class="spacer"></span><a href="/privacy-policy" >Privacy Policy</a><span class="spacer"></span><a href="/terms-of-use" >Terms of Use</a>
+	<a class="aboutlink" href="/about" >About</a><span class="spacer"></span><a href="/privacy-policy" >Privacy Policy</a><span class="spacer"></span><a href="/terms-of-use" >Terms of Use</a>
 </footer>
