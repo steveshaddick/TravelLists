c0968abdd828d8c1a7c113fd08006cae32980123
Charles Law <cablecharlz@gmail.com>
2013-04-16 21:24:46 -0400

    Google Analytics code added in footer.php

--- www/views/parts/footer.php
+++ www/views/parts/footer.php
@@ -1,3 +1,17 @@
 <footer class="footer">
 	<a href="/about" >About</a><span class="spacer"></span><a href="/privacy-policy" >Privacy Policy</a><span class="spacer"></span><a href="/terms-of-use" >Terms of Use</a>
 </footer>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-40205616-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
