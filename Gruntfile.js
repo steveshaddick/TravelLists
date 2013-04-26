60b4497cbaf818e1b271c9c75955b7c192eedd61
Steve Shaddick <steve@steveshaddick.com>
2013-04-26 00:13:41 -0400

    edit grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,9 @@ grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     datetime: Date.now(),
 
-	compass: {                  // Task
-		'uc': {                   // Target
-			options: {              // Target options
+	compass: {
+		d: {
+			options: {
 				sassDir: 'src/scss/',
 				cssDir: 'www/css',
 				imagesDir: 'www/images',
@@ -18,12 +18,33 @@ grunt.initConfig({
     },
 
 	uglify: {
-		options: {
-			preserveComments: 'some',
-			compress: false,
-			beautify: true
+		dev: {
+			options: {
+				preserveComments: 'some',
+				compress: false,
+				beautify: true
+			},
+			files: {
+				'www/js/main.min.js': [
+					'src/js/Location.js',
+					'src/js/Trip.js',
+					'src/js/Edit.js',
+					'src/js/Home.js',
+					'src/js/Main.js'
+				],
+				'www/js/lib.min.js': [
+					'src/js/lib/jquery/jquery-ui-1.9.0.custom.min.js',
+					'src/js/lib/jquery/jquery.cookie.min.js',
+					'src/js/lib/jquery/jquery.dd.js',
+					'src/js/lib/json2.js',
+					'src/js/utility/*.js'
+				]
+			}
 		},
-		'uc': {
+		prod: {
+			options: {
+				preserveComments: 'some'
+			},
 			files: {
 				'www/js/main.min.js': [
 					'src/js/Location.js',
@@ -53,7 +74,7 @@ grunt.initConfig({
 		},
 		scripts: {
 			files: 'src/js/**/*.js',
-			tasks: ['uglify'],
+			tasks: ['uglify:dev'],
 			options: {
 				interrupt: true
 			}
@@ -67,5 +88,6 @@ grunt.loadNpmTasks('grunt-contrib-watch');
 
 // Default task.
 grunt.registerTask('default', ['watch']);
-grunt.registerTask('run', ['compass', 'uglify']);
+grunt.registerTask('dev', ['compass', 'uglify:dev']);
+grunt.registerTask('prod', ['compass', 'uglify:prod']);
 };
