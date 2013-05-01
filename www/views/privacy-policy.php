6cd0569c555e76251fba65052e576b75816bd59d
Charles Law <cablecharlz@gmail.com>
2013-04-30 21:49:21 -0400

    Logo updates

--- www/views/privacy-policy.php
+++ www/views/privacy-policy.php
@@ -5,9 +5,9 @@
 		<div class="contentWrapper">
 		
 			<header class="page-header">
-				<h1 class="site-title"><a href="/">Tripnotes</a></h1>
-				<span class="site-subtitle">The easiest way to get &amp; organize trip suggestions.</span>
-			</header>		
+					<h1 class="site-title"><a href="/"><img src="../images/tripnotes@2x.png" width="398" height="88" alt="Tripnotes"></a></h1>
+					<span class="site-subtitle">The easiest way to get &amp; organize trip suggestions.</span>
+				</header>	
 			
 			
 			<section class="page-content">
